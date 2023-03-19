a12a7b79393cf33d4f4942c4e984e6d4c63e7990
ravid <ravidh610@gmail.com>
2023-03-19 19:37:27 +0200

    third commit

--- Clothe.py
+++ Clothe.py
@@ -1,11 +1,12 @@
 import pygame
 from constans import *
 
+
 class Clothe:
 
     def __init__(self, clothe_width, x_position_cl, y_position_cl, clothe_hieght, img_path_cl):
-            self.clothe_width = clothe_width
-            self.x_position_cl = x_position_cl
-            self.y_position_cl = y_position_cl
-            self.clothe_hieght = clothe_hieght
-            self.img_path_cl = img_path_cl
+        self.clothe_width = clothe_width
+        self.x_position_cl = x_position_cl
+        self.y_position_cl = y_position_cl
+        self.clothe_hieght = clothe_hieght
+        self.img_path_cl = img_path_cl

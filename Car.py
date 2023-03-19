1ba4c7b4a14de81a59bbfb95e54e8013e0e1e74b
ravid <ravidh610@gmail.com>
2023-03-19 19:20:26 +0200

    third commit

---
+++ Car.py
@@ -0,0 +1,12 @@
+import pygame
+from constans import *
+
+class Car:
+
+    def __init__(self,image_path, car_width, x_position_c, y_position_c, car_hieght):
+        self.image_path = 'img/car.jpg'
+        self.car_width = car_width
+        self.x_position_c = x_position_c
+        self.y_position_c = y_position_c
+        self.car_hieght = car_hieght
+
--- Wall.py
+++ Wall.py
@@ -1,6 +1,13 @@
 import pygame
 from constans import *
 
+
 class Wall:
 
-    def __init__(self, ):
+    def __init__(self, image_path, wall_width, x_position_w, y_position_w, wall_hieght):
+        self.image_path = 'img/wall.jpg'
+        self.wall_width = wall_width
+        self.x_position_w = x_position_w
+        self.y_position_w = y_position_w
+        self.wall_height = wall_hieght
+

fef1d5efd860c468289dbd5dd241b12419158f34
ravid <ravidh610@gmail.com>
2023-03-19 19:37:00 +0200

    third commit

--- Wall.py
+++ Wall.py
@@ -4,7 +4,7 @@ from constans import *
 
 class Wall:
 
-    def __init__(self, image_path_w, wall_width, x_position_w, y_position_w, wall_hieght):
+    def __init__(self, wall_width, x_position_w, y_position_w, wall_hieght):
         self.image_path = 'img/wall.jpg'
         self.wall_width = wall_width
         self.x_position_w = x_position_w

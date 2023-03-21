9a41184769b67a20ffabfa195092fbff83fee8b5
EmiliaYoffe <ioffeemilia@gmail.com>
2023-03-21 19:07:20 +0200

    hbx,hm

--- classes/hero.py
+++ classes/hero.py
@@ -20,6 +20,6 @@ class Hero:
     def display_hero(self):
         screen.blit(self.img, (self.x_pos, self.y_pos))
 
-    def lala(self):
+    def move_hero(self, button):
         pass
 
--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finish = True
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                pass
         my_hero.display_hero()
         pygame.display.flip()
 

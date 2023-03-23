158b32f721dd88c73fe2d6291b920883519d37ea
EmiliaYoffe <ioffeemilia@gmail.com>
2023-03-23 18:56:01 +0200

    aded wall

--- Wall.py
+++ Wall.py
@@ -6,9 +6,11 @@ from display import *
 class Wall:
 
     def __init__(self, wall_width, x_position_w, y_position_w, wall_hieght):
-        self.image_path = 'img/wall.jpg'
+        self.image_path = 'img/wall/wall.png'
         self.wall_width = wall_width
         self.x_position_w = x_position_w
         self.y_position_w = y_position_w
         self.wall_height = wall_hieght
 
+    def get_image_path(self):
+        return self.image_path
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pygame
 from constans import *
 from helpers import *
 import math
+from Wall import *
+from display import *
 
 clock = pygame.time.Clock()
 pfs = 60
 
 
 def main():
+    wall_1 = Wall(400, 300, 70, 70)
     bg = pygame.image.load(BG).convert()
     bg = pygame.transform.scale(bg, (WINDOW_WIDTH, WINDOW_HEIGHT))
     from classes.hero import Hero
@@ -36,12 +39,14 @@ def main():
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] and hero_x > 0:
             hero_x -= 5
-        if keys[pygame.K_RIGHT] and hero_x > 0:
+        if keys[pygame.K_RIGHT] and hero_x < WINDOW_WIDTH - HERO_WIDTH:
             hero_x += 5
-        if keys[pygame.K_UP] and hero_x > 0:
+        if keys[pygame.K_UP] and hero_y > 0:
             hero_y -= 5
-        if keys[pygame.K_DOWN] and hero_x > 0:
+        if keys[pygame.K_DOWN] and hero_y < WINDOW_HEIGHT - HERO_HEIGHT:
             hero_y += 5
+
+        add_img(wall_1.get_image_path(), 100, 50, 70, 70)
         my_hero = Hero(hero_x, hero_y, HERO_HEIGHT, HERO_WIDTH)
         my_hero.display_hero()
         pygame.display.flip()

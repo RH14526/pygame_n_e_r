349c260bef3b03c73bbc36859aa01df442872df7
noa <noa0556881258@gmail.com>
2023-03-24 21:29:21 +0300

    bg height 326

--- extrime shit.py
+++ extrime shit.py
@@ -15,3 +15,28 @@ coin_y = random.randint(min_y, max_y)
 # Check if the coin is on the road
 if coin_y >= 400:
     coin_y = coin_y - 100
+
+    import random
+    from typing import List, Tuple
+
+
+    def generate_wall_location(min_x: int, max_x: int, min_y: int, max_y: int, min_dist: int,
+                               existing_walls: List[Tuple[int, int]]) -> Tuple[int, int]:
+        while True:
+            # Generate a random wall location
+            wall_x = random.randint(min_x, max_x)
+            wall_y = random.randint(min_y, max_y)
+
+            # Check if the wall overlaps with any existing walls
+            overlaps = False
+            for loc in existing_walls:
+                if abs(wall_x - loc[0]) < min_dist and abs(wall_y - loc[1]) < min_dist:
+                    overlaps = True
+                    break
+
+            # If the wall doesn't overlap with any existing walls, return its location
+            if not overlaps:
+                return (wall_x, wall_y)
+
+
+

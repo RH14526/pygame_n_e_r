7306ced9200feb351cb05726b7bd05154ee41fd6
ravid <ravidh610@gmail.com>
2023-03-30 14:32:02 +0300

    third commit

--- rules.py
+++ rules.py
@@ -1,22 +1,31 @@
 from helpers import *
 import textwrap
 import pygame
+from Buttens import *
 
 
 def rules():
     pygame.display.set_caption("RULES")
+    from main_menu import main_menu
     while True:
+        mouse = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if go_back_button.check_for_mouse(mouse):
+                    main_menu()
         menu_back = pygame.image.load(MENU_BACK).convert()
         menu_back = pygame.transform.scale(menu_back, (WINDOW_WIDTH, WINDOW_HEIGHT))
         screen.blit(menu_back, (0, 0))
-        RULES_BK = 'img/rules_bk/rules_bk.png'
+        RULES_BK = 'img/rules_bk/rules.png'
         rules_bk = pygame.image.load(RULES_BK).convert()
         rules_bk = pygame.transform.scale(rules_bk, (RULES_WIDTH, RULES_HEIGHT))
         screen.blit(rules_bk, (RULES_X_POSITION, RULES_Y_POSITION))
-        menu_mouse_pos = pygame.mouse.get_pos()
+        go_back_button = pygame.image.load('img/go_back_b/go_back_b.png')
+        back_font = pygame.font.SysFont("Aharoni", 40)
+        go_back_button = Button(go_back_button, (60, 40), " ", back_font, PURPLE, BABY_PINK)
+        go_back_button.update()
         massage = "WELCOME TO FASHION CRAZY!" \
                   " In this incredible fashion race game you can buy cool clothes for your game character with coins, which you can get buy claming them while avoiding the obstclys." \
                   "You have 3 harts at the begging of the game, but you lose 1 everytime that you touch the obstacle." \
